5ffaef68c77a26e022525555cbc051cd8b7fe2dc
Daniel Felipe Zamora Ortiz <D4n7elfelipe@gmail.com>
2021-12-31 12:20:06 -0500

    transaltion page not found

--- src/infraestructura/view/errors/notFound.tsx
+++ src/infraestructura/view/errors/notFound.tsx
@@ -3,14 +3,12 @@ import { Link } from 'react-router-dom';
 import Translate from '../../components/translate';
 
 const Page404 = () => {
-  console.log(123456789);
-
   return (
     <div className="content-page-not-found">
       <Result
         status="404"
         title="404"
-        subTitle="Sorry, the page you visited does not exist."
+        subTitle={Translate('msg404', false)}
         extra={
           <Link className="ant-btn ant-btn-primary" to={'/'}>
             {Translate('home', false)}
